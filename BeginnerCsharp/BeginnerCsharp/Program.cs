27c4757c6db7f3c32b7c1c1cf142ec905a394f10
Ernests Kalnins <ern.kalnins@gmail.com>
2018-01-14 11:12:01 +0200

    Arrays

--- BeginnerCsharp/BeginnerCsharp/Program.cs
+++ BeginnerCsharp/BeginnerCsharp/Program.cs
@@ -8,14 +8,8 @@ namespace BeginnerCsharp
     {
         static void Main(string[] args)
         {
-            Person obj = new Person();
-            obj.firstName = "John";
-            obj.lastName = "Smith";
-            obj.Introduce();
-
-            Calculator calculator = new Calculator();
-            int result = calculator.Add(1, 2);
-            Console.WriteLine(result);
+            Arrays array = new Arrays();
+            array.Initialization();
         }
     }
 }
